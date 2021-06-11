0c442e546db7e078510ea3376f0c7d8fa4b8d4bb
danubiobwm <danubio.bwm@gmail.com>
2021-06-11 18:05:53 -0300

    feat: :truck: Config Routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+const Home: React.FC = () => {
+  return(
+    <>
+    <View>
+      <Text>Home</Text>
+    </View>
+    </>
+  );
+}
+
+export default Home;
